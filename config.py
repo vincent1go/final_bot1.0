cc141f242bae2712cb349b677b1d041bdffac25c
vincent1go <isaievdmitro@icloud.com>
2025-04-27 18:48:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 BOT_TOKEN = "7511704960:AAFKDWgg2-cAzRxywX1gXK47OQRWJi72qGw"
-ADMIN_ID = "ТВОЙ_ID_АДМИНА"  # можешь оставить пустым или позже добавить
+ADMIN_ID = "ТВОЙ_ID_АДМИНА"  # заменишь при необходимости
 WEBHOOK_URL = "https://final-bot1-0.onrender.com/webhook"
 
-# Путь к шаблонам PDF
+# Путь к шаблонам DOCX (обрати внимание — теперь docx)
 TEMPLATES = {
-    "UR Recruitment LTD": "clean_template_no_text.pdf",
-    "SMALL WORLD RECRUITMENT LTD": "template_small_world.pdf",
-    "MILA CONSULTANTS LTD": "contract_template3.pdf"
+    "UR Recruitment LTD": "template_ur.docx",
+    "Small World Recruitment LTD": "template_small_world.docx",
+    "Imperative LTD": "template_imperative.docx"
 }
