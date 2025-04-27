f01cfce3884d4de13ad722f7b6c7ed4f393dcc41
vincent1go <isaievdmitro@icloud.com>
2025-04-27 19:22:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
-BOT_TOKEN = "7511704960:AAFKDWgg2-cAzRxywX1gXK47OQRWJi72qGw"
-ADMIN_ID = ""  # Можешь позже добавить ID для оповещений
-WEBHOOK_URL = "https://final-bot1-0.onrender.com/webhook"
+# config.py
 
+BOT_TOKEN = "7511704960:AAFKDWgg2-cAzRxywX1gXK47OQRWJi72qGw"
+WEBHOOK_URL = "https://final-bot1-0.onrender.com/webhook"
+ADMIN_ID = ""  # Можно оставить пустым
+
+# Путь к шаблонам DOCX
 TEMPLATES = {
     "UR Recruitment LTD": "templates/template_ur.docx",
     "SMALL WORLD RECRUITMENT LTD": "templates/template_small_world.docx",
-    "IMPERATIVE Recruitment LTD": "templates/template_imperative.docx"
+    "IMPERATIVE CONSULTANTS LTD": "templates/template_imperative.docx"
 }
