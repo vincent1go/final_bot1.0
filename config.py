f35d928d864274830b9742a25d9eadcf231cb92d
vincent1go <isaievdmitro@icloud.com>
2025-04-27 18:40:52 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+BOT_TOKEN = "7511704960:AAFKDWgg2-cAzRxywX1gXK47OQRWJi72qGw"
+ADMIN_ID = "ТВОЙ_ID_АДМИНА"  # можешь оставить пустым или позже добавить
+WEBHOOK_URL = "https://final-bot1-0.onrender.com/webhook"
+
+# Путь к шаблонам PDF
+TEMPLATES = {
+    "UR Recruitment LTD": "clean_template_no_text.pdf",
+    "SMALL WORLD RECRUITMENT LTD": "template_small_world.pdf",
+    "MILA CONSULTANTS LTD": "contract_template3.pdf"
+}
