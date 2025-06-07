741a1e9c16813578a417fb07fcbad96253c9e91f
vincent1go <isaievdmitro@icloud.com>
2025-06-08 00:27:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -137,4 +137,5 @@ if __name__ == "__main__":
     app.on_startup.append(on_startup)
     app.on_shutdown.append(on_shutdown)
 
-    web.run_app(app, host="0_
+    web.run_app(app, host="0.0.0.0", port=PORT)
+
